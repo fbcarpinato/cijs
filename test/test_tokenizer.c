f3acc90f73fe50e12be65dac91f491614531dd13
Fabio Carpinato <fabiocarpinato@outlook.it>
2025-01-27 17:37:10 +0100

    implements a simple test for the tokenizer

--- test/test_tokenizer.c
+++ test/test_tokenizer.c
@@ -1,6 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "../include/tokenizer.h"
 #include "../third_party/Unity/src/unity.h"
 
 void setUp(void) {}
 void tearDown(void) {}
 
-void test_tokenizer_basic(void) { TEST_ASSERT_EQUAL_INT(0, 0); }
+void test_tokenizer_basic(void) {
+  Tokenizer *tokenizer = (Tokenizer *)malloc(sizeof(Tokenizer));
+
+  TokenizerInitError tokenizer_init_error =
+      init_tokenizer(tokenizer, "let test = 1;");
+
+  if (tokenizer_init_error != TOKENIZER_INIT_OK) {
+    TEST_ASSERT_EQUAL_INT(1, 0);
+  }
+
+  unsigned int token_count = 0;
+
+  const char *token;
+  do {
+    token = next_token(tokenizer);
+
+    free((void *)token);
+
+    token_count++;
+  } while (token);
+
+  free(tokenizer);
+
+  TEST_ASSERT_EQUAL_INT(5, token_count);
+}
